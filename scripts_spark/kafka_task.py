e20f6d23f83a604d50b0c9403b195740c25f236c
root <root@locust-master>
2017-10-03 20:19:13 +0000

    add options for spark script, pep8, better helper.txt

--- scripts_spark/kafka_task.py
+++ scripts_spark/kafka_task.py
@@ -7,39 +7,74 @@ from kafka import KafkaProducer
 
 import sys
 import json
+import argparse
 
+parser = argparse.ArgumentParser(
+    description="Pipeline Kafka-Spark. \
+        Get data from Kafka topic, remove field 'fields' from json \
+        and return to other Kafka topic."
+)
 
-TOPIC_IN = "Inbound"
-TOPIC_OUT = "Outbound"
-BROKER = "127.0.0.1:9092"
+parser.add_argument(
+    "--topic_in",
+    dest="topic_in",
+    metavar="Inbound",
+    type=str, 
+    help="Kafka topic from where data will deliver to Spark",
+    required=True
+)
+
+parser.add_argument(
+    "--topic_out",
+    dest="topic_out",
+    metavar="Outbound",
+    type=str, 
+    help="Kafka topic for storing changed value (for producer)",
+    required=True
+)
+
+parser.add_argument(
+    "--broker",
+    dest="broker",
+    metavar="127.0.0.1:9092",
+    type=str,
+    help="Kafka broker ip:port (Default: 127.0.0.1:9092)",
+    default="127.0.0.1:9092"
+)
+
+args = parser.parse_args()
+
+TOPIC_IN = args.topic_in
+TOPIC_OUT = args.topic_out
+BROKER = args.broker
 
 
 def sendToBroker(json_records):
-  producer = KafkaProducer(bootstrap_servers=[BROKER])
-  producer.send(TOPIC_OUT, json_records)
-  producer.flush()
+    producer = KafkaProducer(bootstrap_servers=[BROKER])
+    producer.send(TOPIC_OUT, json_records)
+    producer.flush()
 
 def handler(rdd_mapped):
-  records = rdd_mapped.collect()
-  records_str = ""
+    records = rdd_mapped.collect()
+    records_str = ""
 
-  for record in records:
-    records_str = records_str + str(record['payload']) + "\n"
+    for record in records:
+      records_str = records_str + str(record['payload']) + "\n"
 
-  json_records = json.loads(records_str)
+    json_records = json.loads(records_str)
   
-  # filter out "fields" field
-  json_records.pop('fields', None)
+    # filter out "fields" field
+    json_records.pop('fields', None)
 
-  sendToBroker(json.dumps(json_records, indent=2))
+    sendToBroker(json.dumps(json_records, indent=2))
 
 
 if __name__ == "__main__":
-  sc = SparkContext(appName="Kafka")
-  sc.setLogLevel("WARN")
+    sc = SparkContext(appName="Kafka")
+    sc.setLogLevel("WARN")
   
-  offset = OffsetRange(TOPIC_IN, 0, 0, 16)
-  rdd = KafkaUtils.createRDD(sc, {"metadata.broker.list": BROKER}, [offset])
-  rdd_mapped = rdd.map(lambda v: json.loads(v[1]))
-  handler(rdd_mapped)
+    offset = OffsetRange(TOPIC_IN, 0, 0, 16)
+    rdd = KafkaUtils.createRDD(sc, {"metadata.broker.list": BROKER}, [offset])
+    rdd_mapped = rdd.map(lambda v: json.loads(v[1]))
+    handler(rdd_mapped)
 
